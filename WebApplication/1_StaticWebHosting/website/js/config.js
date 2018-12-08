d6ccac79fbcbd1e2fc14086967027323516bc416
jagadishgowda1996 <45556477+jagadishgowda1996@users.noreply.github.com>
2018-12-09 00:31:03 +0530

    Update config.js
    changes done

--- WebApplication/1_StaticWebHosting/website/js/config.js
+++ WebApplication/1_StaticWebHosting/website/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_DXMaaOtQM', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'Jhjho: 2ub8mui75afc16edvd3v7s4s0a', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-1.amazonaws.com/prod',
     }
 };
